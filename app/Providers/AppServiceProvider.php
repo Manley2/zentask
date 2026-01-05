0744688ed849f359ad1fd509e8bfa24c8710bad3
Kevin <kevinarkananta39@gmail.com>
2026-01-05 16:19:15 +0700

    update project

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Pastikan URL generator konsisten HTTPS jika APP_URL pakai https (mis. ngrok)
+        // Ini mencegah redirect/asset jadi http:// saat sebenarnya akses https://
+        if (!app()->runningInConsole()) {
+            $appUrl = (string) config('app.url');
+
+            if (Str::startsWith($appUrl, 'https://')) {
+                URL::forceScheme('https');
+            }
+        }
     }
 }
